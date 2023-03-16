fee5c635953af0742fc5735e2a4abccbef23d8b8
Armance Mousset <mousset.armance@gmail.com>
2023-03-16 16:22:25 -0400

    Add navigation bar component and styling as well as the assets folder for icons

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,10 +1,52 @@
+import { NavLink } from "react-router-dom";
+
+// icons
+import logo from "../../assets/icons/instagram__logo.png";
+import homeIcon from "../../assets/icons/home.png";
+import searchIcon from "../../assets/icons/search.png";
+import exploreIcon from "../../assets/icons/explore.png";
+import messagesIcon from "../../assets/icons/message.png";
+import notificationsIcon from "../../assets/icons/notifications.png";
+import createIcon from "../../assets/icons/create.png";
+
+// stylesheet
+import "./Navbar.scss";
+
 function Navbar() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="navigation">
+      <img className="navigation__logo" src={logo} />
+      <div className="navigation__list">
+        <NavLink className="navigation__container">
+          <img className="navigation__icon" src={homeIcon} />
+          <h3 className="navigation__item navigation__item--active">Home</h3>
+        </NavLink>
+        <NavLink className="navigation__container">
+          <img className="navigation__icon" src={searchIcon} />
+          <h3 className="navigation__item">Search</h3>
+        </NavLink>
+        <NavLink className="navigation__container">
+          <img className="navigation__icon" src={exploreIcon} />
+          <h3 className="navigation__item">Explore</h3>
+        </NavLink>
+        <NavLink className="navigation__container">
+          <img className="navigation__icon" src={messagesIcon} />
+          <h3 className="navigation__item">Messages</h3>
+        </NavLink>
+        <NavLink className="navigation__container">
+          <img className="navigation__icon" src={notificationsIcon} />
+          <h3 className="navigation__item">Notification</h3>
+        </NavLink>
+        <NavLink className="navigation__container">
+          <img className="navigation__icon" src={createIcon} />
+          <h3 className="navigation__item">Create</h3>
+        </NavLink>
+        <NavLink className="navigation__container">
+          <img className="navigation__icon" />
+          <h3 className="navigation__item">Profile</h3>
+        </NavLink>
+      </div>
+    </div>
   );
 }
 
