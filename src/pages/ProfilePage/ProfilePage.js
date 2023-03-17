fd35985f0badd7b8bbd3c758e7ee7e76e933f0df
Armance Mousset <mousset.armance@gmail.com>
2023-03-17 09:13:52 -0400

    changes

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,6 +1,9 @@
+// stylesheet
+import "./ProfilePage.scss";
+
 function ProfilePage() {
   return (
-    <>
+    <div className="profile">
       <div className="profile__info">
         <img classname="profile__avatar" />
         <h2 className="profile__username">Patrick</h2>
@@ -16,10 +19,8 @@ function ProfilePage() {
             @tinyhomesnew 🌲🏠 * Tiny Home Plans now available!
           </p>
         </div>
-
-        <div className="profile__bio"></div>
       </div>
-    </>
+    </div>
   );
 }
 
